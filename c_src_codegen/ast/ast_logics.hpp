fdf5e8a1644d2a05778dfde322131048f55e9014
Enda Mulville <em1716@eews305-047.ee.ic.ac.uk>
2018-03-12 18:12:02 +0000

    rename making problems

---
+++ c_src_codegen/ast/ast_logics.hpp
@@ -0,0 +1,252 @@
+#ifndef ast_logics_hpp
+#define ast_logics_hpp
+
+#include <string>
+#include <iostream>
+
+class Logic
+	: public Statement
+{
+protected:
+
+	
+public:
+	StatementPtr left;
+	StatementPtr right;
+
+	Logic(StatementPtr _left, StatementPtr _right) :
+	left(_left)
+	, right(_right) {}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{}
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{}
+
+	std::string getId() const override{
+		return left->getId();
+	}
+
+	double getValue() const override
+    	{ 
+		return right->getValue();
+	}
+};
+
+class Equal
+	: public Logic
+{
+public:
+	
+	Equal(StatementPtr _left, StatementPtr _right)
+	: Logic(_left, _right)
+	{}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << "==";
+		right->translate(dst, scope, scope_bindings);
+	}
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << "==";
+		right->translate(dst, scope, scope_bindings);
+	}
+	
+};
+
+class NotEqual
+	: public Logic
+{
+public:
+	
+	NotEqual(StatementPtr _left, StatementPtr _right)
+	: Logic(_left, _right)
+	{}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << "!=";
+		right->translate(dst, scope, scope_bindings);
+	}
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << "!=";
+		right->translate(dst, scope, scope_bindings);
+	}
+	
+};
+
+class GreaterThan
+	: public Logic
+{
+public:
+	
+	GreaterThan(StatementPtr _left, StatementPtr _right)
+	: Logic(_left, _right)
+	{}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << ">";
+		right->translate(dst, scope, scope_bindings);
+	}
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << ">";
+		right->translate(dst, scope, scope_bindings);
+	}
+	
+};
+
+class GreaterThanOrEqual
+	: public Logic
+{
+public:
+	
+	GreaterThanOrEqual(StatementPtr _left, StatementPtr _right)
+	: Logic(_left, _right)
+	{}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << ">=";
+		right->translate(dst, scope, scope_bindings);
+	}
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << ">=";
+		right->translate(dst, scope, scope_bindings);
+	}
+	
+};
+
+class LessThan
+	: public Logic
+{
+public:
+	
+	LessThan(StatementPtr _left, StatementPtr _right)
+	: Logic(_left, _right)
+	{}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << "<";
+		right->translate(dst, scope, scope_bindings);
+	}
+	
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << "<";
+		right->translate(dst, scope, scope_bindings);
+	}
+};
+
+class LessThanOrEqual
+	: public Logic
+{
+public:
+	
+	LessThanOrEqual(StatementPtr _left, StatementPtr _right)
+	: Logic(_left, _right)
+	{}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << "<=";
+		right->translate(dst, scope, scope_bindings);
+	}
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << "<=";
+		right->translate(dst, scope, scope_bindings);
+	}
+	
+	
+};
+
+class And
+	: public Logic
+{
+public:
+	
+	And(StatementPtr _left, StatementPtr _right)
+	: Logic(_left, _right)
+	{}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		left->translate(dst, scope, scope_bindings);
+		dst << "&&";
+		right->translate(dst, scope, scope_bindings);
+	}
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		left->translate(dst, scope, scope_bindings);
+		dst << "&&";
+		right->translate(dst, scope, scope_bindings);
+	}
+	
+};
+
+class Or
+	: public Logic
+{
+public:
+	
+	Or(StatementPtr _left, StatementPtr _right)
+	: Logic(_left, _right)
+	{}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << "||";
+		right->translate(dst, scope, scope_bindings);
+	}
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		
+		left->translate(dst, scope, scope_bindings);
+		dst << "||";
+		right->translate(dst, scope, scope_bindings);
+	}
+	
+};
+
+
+#endif
---
+++ c_src_codegen/ast/ast_statement.hpp
@@ -0,0 +1,153 @@
+#ifndef ast_statement_hpp
+#define ast_statement_hpp
+
+#include <string>
+#include <iostream>
+#include <map>
+
+#include <memory>
+
+class Statement;
+
+
+
+typedef const Statement *StatementPtr;
+
+class Statement
+{
+public:
+	virtual ~Statement(){}	
+
+	int scope;
+
+	struct meta_data 
+	{
+		int var_scope;
+		int stack_address;
+	}
+
+
+	//! Tell and Statement to translate to stream
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const =0;
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const =0;
+
+	virtual std::string getId() const { return "error";}
+	virtual double getValue() const { return 99999;}
+	//! Evaluate the tree using the given mapping of variables to numbers
+	
+	std::string makeName(std::string name, int scope){
+		name += "_" + scope;
+		return name;
+	}
+		
+};
+
+
+class StatementPair
+	: public Statement
+{
+public:
+
+	StatementPtr this_statement;
+	StatementPtr next = NULL;
+
+	StatementPair(StatementPtr _this_statement, StatementPtr _next) :
+		this_statement(_this_statement)
+		,next(_next)
+	{}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		this_statement->translate(dst, scope, scope_bindings);
+		if (next != NULL)
+		{	
+			for(int x = 0; x < scope; x++){
+				dst << "    ";
+			}
+			next->translate(dst, scope, scope_bindings);
+
+			
+		} 
+	}
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		this_statement->compile(dst, scope, scope_bindings);
+		if (next != NULL)
+		{	
+			next->compile(dst, scope, scope_bindings);	
+		} 
+	}
+};
+
+class ArgumentPair
+	: public Statement
+{
+public:
+
+	StatementPtr this_statement;
+	StatementPtr next = NULL;
+
+	ArgumentPair(StatementPtr _this_statement, StatementPtr _next) :
+		this_statement(_this_statement)
+		,next(_next)
+	{}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		this_statement->translate(dst, scope, scope_bindings);
+		if (next != NULL)
+		{
+			dst << ",";
+			next->translate(dst, scope, scope_bindings);
+		}
+	}
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		this_statement->translate(dst, scope, scope_bindings);
+		if (next != NULL)
+		{
+			dst << ",";
+			next->translate(dst, scope, scope_bindings);
+		}
+	}
+
+};
+
+class ProgramPair
+	: public Statement
+{
+public:
+
+	StatementPtr this_statement;
+	StatementPtr next = NULL;
+
+	ProgramPair(StatementPtr _this_statement, StatementPtr _next) :
+		this_statement(_this_statement)
+		,next(_next)
+	{}
+
+	virtual void translate(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		this_statement->translate(dst, scope, scope_bindings);
+		if (next != NULL)
+		{
+			next->translate(dst, scope, scope_bindings);
+			dst << std::endl;
+		}
+	}
+
+	virtual void compile(std::ostream &dst, int &scope, std::map<std::string,double> &scope_bindings) const override
+	{
+		this_statement->compile(dst, scope, scope_bindings);
+		if (next != NULL)
+		{
+			next->compile(dst, scope, scope_bindings);
+			dst << std::endl;
+		}
+	}
+};
+
+#endif
